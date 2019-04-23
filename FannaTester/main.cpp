0cfa10aa72244cc1b7daec5ffed671eeadb018cb
daeds1dog <41439659+daeds1dog@users.noreply.github.com>
2019-04-24 00:27:22 +0300

    Define testing functions

--- FannaTester/main.cpp
+++ FannaTester/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <string>
 #include <Windows.h>
 #include <libloaderapi.h>
 
-typedef int(__cdecl* MYPROC)();
+#define PI std::string pair, std::string interval, double* opening_price, double* closing_price, double *max_price, double *min_price, double* volume
+
+
+typedef void(__cdecl* VOIDPROC)(PI);
+typedef void(__cdecl* VOIDPROCSEC)(PI, int samples);
+typedef float(__cdecl* FLOATPROC)(PI);
 
 int main(void) {
 	HINSTANCE lib = LoadLibrary("Fanna.dll");
-	MYPROC ProcAdd = (MYPROC)GetProcAddress(lib, "test");
-	(ProcAdd)();
+	VOIDPROC reset_fanna = (VOIDPROC)GetProcAddress(lib, "reset_fanna");
+	VOIDPROC train_fanna = (VOIDPROC)GetProcAddress(lib, "train_fanna");
+	VOIDPROCSEC build_fanna_database = (VOIDPROCSEC)GetProcAddress(lib, "build_fanna_database");
+	FLOATPROC pulse_fanna = (FLOATPROC)GetProcAddress(lib, "pulse_fanna");
 	system("pause");
 }
